d85b75e15a1effda9e05a5bc736ef0d244dd8cb7
luccasgois <luccasgois1@gmail.com>
2024-05-01 00:22:44 -0300

    Add purchage scenarios happy and sad path

--- projects/starbugs-cucumber/features/coffeeCatalog.feature
+++ projects/starbugs-cucumber/features/coffeeCatalog.feature
@@ -5,3 +5,18 @@ Feature: Coffee Catalog
 Scenario: Access the Coffee Catalog on Main page
     When The User access the main page of StarBugs
     Then The User should see the available coffee options
+
+Scenario: Start a purchase of a coffee
+    Given The User is on the main page of StartBugs
+        And The User wants to buy a "Expresso Gelado"
+        And The Product costs "R$ 9,99"
+        And The Delivery cost is "R$ 10,00"
+    When The User clicks to buy the coffee
+    Then The User goes to Checkout page with the details of the Product
+        And The total cost must be "R$ 19,99"
+
+Scenario: Start a purchase of a unavailable coffee
+    Given The User is on the main page of StartBugs
+        And The User wants to buy a "Expresso Cremoso"
+    When The User clicks to buy the coffee
+    Then The User sees a popup for unavailable product
--- projects/starbugs-cucumber/features/step_definitions/coffeeCatalog.steps.rb
+++ projects/starbugs-cucumber/features/step_definitions/coffeeCatalog.steps.rb
@@ -6,3 +6,46 @@ Then('The User should see the available coffee options') do
     coffees = all('.coffee-item')
     expect(coffees.size).to be > 0
 end
+
+Given('The User is on the main page of StartBugs') do
+    visit 'https://starbugs.vercel.app'
+end
+
+Given('The User wants to buy a {string}') do |product_name|
+    @product_name = product_name
+end
+
+Given('The Product costs {string}') do |product_cost|
+    @product_cost = product_cost
+end
+
+Given('The Delivery cost is {string}') do |product_delivery_cost|
+    @product_delivery_cost = product_delivery_cost
+end
+
+When('The User clicks to buy the coffee') do
+    product = find('.coffee-item', text: @product_name)
+    product.find('.buy-coffee').click
+end
+
+Then('The User goes to Checkout page with the details of the Product') do
+    product_title = find('.item-details h1')
+    expect(product_title.text).to eql @product_name
+
+    sub_cost = find('.subtotal .sub-price')
+    expect(sub_cost.text).to eql @product_cost
+
+    delivery_price = find('.delivery-price')
+    expect(delivery_price.text).to eql @product_delivery_cost
+end
+
+Then('The total cost must be {string}') do |total_cost|
+    total_price = find('.total-price')
+    expect(total_price.text).to eql total_cost
+end
+
+Then('The User sees a popup for unavailable product') do
+    expected_popup_message = 'Produto indisponível'
+    popup_warning = find('[role="dialog"]')
+    expect(popup_warning.text).to include(expected_popup_message)
+end
