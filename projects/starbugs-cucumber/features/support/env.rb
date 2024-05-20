4219828a893512ce8751a1c48f46ad533112895d
luccasgois <luccasgois1@gmail.com>
2024-05-20 18:20:07 -0300

    Add docker environment for tests

--- projects/starbugs-cucumber/features/support/env.rb
+++ projects/starbugs-cucumber/features/support/env.rb
@@ -2,6 +2,6 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 
 Capybara.configure do |config|
-    config.default_driver = :selenium
+    config.default_driver = ENV["HEADLESS_MODE"].to_s.downcase == "true" ? :selenium_headless : :selenium
     config.default_max_wait_time = 5
 end
